241bc366450fdaff3dfae0e10fda3babe64206c1
GentiliEmanuele <egentili2000@gmail.com>
2025-08-18 15:05:15 +0200

    Modified HyperExponential

--- src/it/uniroma2/models/distr/HyperExponential.java
+++ src/it/uniroma2/models/distr/HyperExponential.java
@@ -7,20 +7,25 @@ public class HyperExponential extends Distribution {
     private final Exponential exp2;
     private final Bernoulli bernoulli;
 
-    public HyperExponential(Rngs r, int stream, double mu1, double mu2, Bernoulli bernoulli) {
-        super(r, stream);
-        this.bernoulli = bernoulli;
-        this.exp1 = new Exponential(r, stream, mu1);
-        this.exp2 = new Exponential(r, stream, mu2);
+    public HyperExponential(Rngs r, int stream1, double mu1, int stream2, double mu2, int stream3, double p) {
+        super(r, 0);
+        this.bernoulli = new Bernoulli(r, stream3, p);
+        this.exp1 = new Exponential(r, stream1, mu1);
+        this.exp2 = new Exponential(r, stream2, mu2);
+    }
+
+    @Override
+    public double gen() {
+        var x = this.bernoulli.gen();
+        if (x == 1.0) {
+            return this.exp1.gen();
+        } else {
+            return this.exp2.gen();
+        }
     }
 
     @Override
     protected double newTime() {
-        var x = this.bernoulli.newTime();
-        if (x == 1.0) {
-            return this.exp1.newTime();
-        } else {
-            return this.exp2.newTime();
-        }
+        throw new UnsupportedOperationException("Not supported yet.");
     }
 }
