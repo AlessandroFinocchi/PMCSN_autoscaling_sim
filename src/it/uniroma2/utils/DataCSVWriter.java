9213d592342c01f3debd12daa94b96da84279cb9
gmarseglia <g.marseglia.it@gmail.com>
2025-08-26 17:48:16 +0200

    Better CSV files for transitory analysis of base model

--- src/it/uniroma2/utils/DataCSVWriter.java
+++ src/it/uniroma2/utils/DataCSVWriter.java
@@ -26,10 +26,10 @@ public class DataCSVWriter {
     public static final DataTimeTable INTER_RUN_DATA = new DataTimeTable();
     public static final Double INTER_RUN_KEY = -1.0;
 
-    private static final String OUT_DIR_PATH = "out_data/" + OUT_DIR_PATH_SUFFIX();
+    private static final String OUT_DIR_PATH = "out_data";
+    private static final String OUT_DIR_PATH_WITH_SUFFIX = OUT_DIR_PATH + "/" + OUT_DIR_PATH_SUFFIX();
 
     private static String OUT_DIR_PATH_SUFFIX() {
-        // return "";
         return START_DATE_TIME.getYear() + "-" + START_DATE_TIME.getMonthValue() + "-" + START_DATE_TIME.getDayOfMonth() + "_" + START_DATE_TIME.getHour() + "-" + START_DATE_TIME.getMinute() + "-" + START_DATE_TIME.getSecond();
     }
 
@@ -53,11 +53,11 @@ public class DataCSVWriter {
      * @param fileName  The name of the file in the OUT_DIR_PATH.
      * @param fields    The lists of the fields that will be flushed on file.
      */
-    public static void flushList(DataTimeTable timeTable, String fileName, String[] fields, boolean append) {
-        String filePath = OUT_DIR_PATH + "/" + fileName + ".csv";
+    public static void flushList(DataTimeTable timeTable, String outPath, String fileName, String[] fields, boolean append) {
+        String filePath = outPath + "/" + fileName + ".csv";
 
         // Create the directory if not exists
-        File directory = new File(OUT_DIR_PATH);
+        File directory = new File(OUT_DIR_PATH_WITH_SUFFIX);
         if (!directory.exists() && !directory.mkdirs()) {
             throw new RuntimeException("Failed to create output directory");
         }
@@ -85,21 +85,15 @@ public class DataCSVWriter {
         }
     }
 
-    public static void flushList(DataTimeTable timeTable, String fileName, DataField[] fields, boolean append) throws IOException {
-        String[] strFields = new String[fields.length];
-        for (int i = 0; i < fields.length; i++) {
-            strFields[i] = fields[i].toString();
-        }
-        flushList(timeTable, fileName, strFields, append);
-    }
-
     public static void flushAllIntra(RunConfiguration c, int repetition) {
         String fileNameSuffix = "-" + c.getName() + "_" + repetition;
 
         /* Log data about scaling events */
         DataHeaders scalingHeaders = new DataHeaders(TIMESTAMP, R_0, MOVING_R_O, EVENT_TYPE, TO_BE_ACTIVE, ACTIVE, TO_BE_REMOVED, REMOVED);
         DataTimeTable filteredScalingData = INTRA_RUN_DATA.filter(EVENT_TYPE, false, "ARRIVAL").filter(EVENT_TYPE, false, "COMPLETION");
-        flushList(filteredScalingData, "scaling" + fileNameSuffix, scalingHeaders.get(), false);
+        flushList(filteredScalingData,
+                OUT_DIR_PATH_WITH_SUFFIX, "scaling" + fileNameSuffix,
+                scalingHeaders.get(), false);
 
         /* Log data about jobs in each server */
         DataHeaders jobsHeaders = new DataHeaders();
@@ -113,7 +107,9 @@ public class DataCSVWriter {
             jobsHeaders.add("JOBS_IN_SERVER_" + i);
         }
         DataTimeTable filteredJobsData = INTRA_RUN_DATA.filter(EVENT_TYPE, false, "ACTIVE");
-        flushList(filteredJobsData, "jobs" + fileNameSuffix, jobsHeaders.get(), false);
+        flushList(filteredJobsData,
+                OUT_DIR_PATH_WITH_SUFFIX, "jobs" + fileNameSuffix,
+                jobsHeaders.get(), false);
 
         /* Log data about jobs in each server */
         DataHeaders allJobsHeaders = new DataHeaders();
@@ -126,11 +122,12 @@ public class DataCSVWriter {
             allJobsHeaders.add("STATUS_OF_SERVER_" + i);
             allJobsHeaders.add("JOBS_IN_SERVER_" + i);
         }
-        allJobsHeaders.add(CONFIGURATION_ID, REPETITION_ID);
+        allJobsHeaders.add(REPETITION_ID);
         DataTimeTable combinedJobsData = filteredJobsData
-                .setEach(CONFIGURATION_ID, c.getName())
                 .setEach(REPETITION_ID, repetition);
-        flushList(combinedJobsData, "jobs" + "-all", allJobsHeaders.get(), true);
+        flushList(combinedJobsData,
+                OUT_DIR_PATH_WITH_SUFFIX, "jobs" + "-" + c.getName() + "_all",
+                allJobsHeaders.get(), true);
 
         INTRA_RUN_DATA.clear();
     }
@@ -141,6 +138,11 @@ public class DataCSVWriter {
             INTER_RUN_DATA_HEADERS.add(STREAM_SEED + "_" + stream);
         }
         INTER_RUN_DATA_HEADERS.add(RUN_DATETIME, CONFIGURATION_ID, REPETITION_ID, FINAL_TS, TOTAL_ALLOCATED_CAPACITY, TOTAL_ALLOCATED_CAPACITY_PER_SEC, SYSTEM_UTILIZATION, MEAN_SYSTEM_RESPONSE_TIME, TOTAL_JOBS_COMPLETED, TOTAL_SPIKE_JOBS_COMPLETED, TOTAL_SLO_VIOLATIONS, SLO_VIOLATIONS_PERCENTAGE);
-        flushList(INTER_RUN_DATA, "final", INTER_RUN_DATA_HEADERS.get(), true);
+        flushList(INTER_RUN_DATA,
+                OUT_DIR_PATH, "final_all",
+                INTER_RUN_DATA_HEADERS.get(), true);
+        flushList(INTER_RUN_DATA,
+                OUT_DIR_PATH_WITH_SUFFIX, "final",
+                INTER_RUN_DATA_HEADERS.get(), true);
     }
 }
