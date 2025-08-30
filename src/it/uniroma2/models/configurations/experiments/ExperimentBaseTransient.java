c24c02a8f86c37da8274feeecdef0b7609949299
AlessandroFinocchi <alessandro.22082001@gmail.com>
2025-08-30 15:55:53 +0200

    fixed bug when adding cv to exp_trans_b

--- src/it/uniroma2/models/configurations/experiments/ExperimentBaseTransient.java
+++ src/it/uniroma2/models/configurations/experiments/ExperimentBaseTransient.java
@@ -22,33 +22,34 @@ public class ExperimentBaseTransient implements Experiment {
             result.add(c);
         }
 
-        setConfiguration(1, 4.0, 1, 4, 3, false, null);
-        setConfiguration(2, 4.0, 1, 4,5, false, null);
-        setConfiguration(3, 4.0, 1, 4,8, false, null);
-        setConfiguration(4, 4.8, 1, 4,5, false, null);
-        setConfiguration(5, 10, 0.4, 4,5, false, null);
-        setConfiguration(6, 2, 2, 4,5, false, null);
-        setConfiguration(7, 10, 0.4, 16,8, false, null);
-        setConfiguration(8, 4, 1, 4,5, true, 0.1);
-        setConfiguration(9, 4, 1, 4,5, true, 0.3);
-        setConfiguration(10, 4, 1, 4,5, true, 1.0);
-        setConfiguration(11, 4, 1, 4,5, true, 100.0);
-        setConfiguration(12, 4, 1.5, 4,5, true, 2.0);
-        setConfiguration(13, 4, 1.5, 4,5, true, 4.0);
-        setConfiguration(14, 4, 1.5, 4,5, true, 10.0);
-        setConfiguration(15, 4, 1.5, 4,5, true, 20.0);
+        setConfiguration(1, 4.0, 1, 3, false, null);
+        setConfiguration(2, 4.0, 1,5, false, null);
+        setConfiguration(3, 4.0, 1,8, false, null);
+        setConfiguration(4, 4.8, 1,5, false, null);
+        setConfiguration(5, 10, 0.4,5, false, null);
+        setConfiguration(6, 2, 2,5, false, null);
+        setConfiguration(7, 10, 16,8, false, null);
+        setConfiguration(8, 4, 1,5, true, 0.1);
+        setConfiguration(9, 4, 1,5, true, 0.3);
+        setConfiguration(10, 4, 1,5, true, 1.0);
+        setConfiguration(11, 4, 1,5, true, 100.0);
+        setConfiguration(12, 4, 1.5,5, true, 2.0);
+        setConfiguration(13, 4, 1.5,5, true, 4.0);
+        setConfiguration(14, 4, 1.5,5, true, 10.0);
+        setConfiguration(15, 4, 1.5,5, true, 20.0);
 
         return result;
     }
 
-    void setConfiguration(int index, double lambda, double z, double cv, int wsNumber, boolean ssActive, Double siMax) {
+    void setConfiguration(int index, double lambda, double z, int wsNumber, boolean ssActive, Double siMax) {
         RunConfiguration c = result.get(index - 1);
         c.put("distribution.arrivals.mu", String.valueOf(1.0 / lambda));
-        c.put("distribution.arrivals.cv", String.valueOf(1.0 / lambda));
         c.put("distribution.services.z", String.valueOf(z));
-        c.put("distribution.services.cv", String.valueOf(cv));
         c.put("infrastructure.start_num_server", String.valueOf(wsNumber));
         c.put("infrastructure.spikeserver.active", String.valueOf(ssActive));
         if (siMax != null) c.put("infrastructure.si_max", String.valueOf(siMax));
+
+        // extra
+        c.put("infrastructure.max_num_server", String.valueOf(wsNumber));
     }
 }
