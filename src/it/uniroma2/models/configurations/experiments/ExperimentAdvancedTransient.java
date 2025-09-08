33643c224850aef2d7d796ed9851536355e8ccf4
gmarseglia <g.marseglia.it@gmail.com>
2025-09-08 19:37:11 +0200

    Completed transient analysis for advanced model

--- src/it/uniroma2/models/configurations/experiments/ExperimentAdvancedTransient.java
+++ src/it/uniroma2/models/configurations/experiments/ExperimentAdvancedTransient.java
@@ -7,66 +7,51 @@ import java.util.List;
 
 public class ExperimentAdvancedTransient implements Experiment {
     List<RunConfiguration> result = new ArrayList<>();
-    private int i;
+    private int index;
 
     @Override
     public List<RunConfiguration> getRunConfigurations() {
-        // // Group 1
-        // i = 1;
-        // addConfiguration(i++, 4.0, 1.0, 4, 10, null, 0.2, null);
-        // addConfiguration(i++, 4.0, 1.0, 4, 10, null, 5.0, null);
-        // addConfiguration(i++, 4.0, 1.0, 4, 10, null, 100.0, null);
-        // // Group 2
-        // i = 4;
-        // addConfiguration(i++, 4.0, 1.0, 4, 10, 3.0, 0.2, null);
-        // addConfiguration(i++, 4.0, 1.0, 4, 10, 3.0, 5.0, null);
-        // addConfiguration(i++, 4.0, 1.0, 4, 10, 3.0, 100.0, null);
-        // Group 3
-        i = 7;
-        addConfiguration(i++, 8.0, 1.0, 4, 10, null, 0.1, null);
-        addConfiguration(i++, 8.0, 1.0, 4, 10, 3.0, 5.0, null);
+        /*  */
+        index = 100;
+        setConfiguration(++index, 10, 2.0, 3.0, "r0");
+        setConfiguration(++index, 10, 2.0, 3.0, "jobs");
+
+        index = 200;
+        setConfiguration(++index, 6, 2.0, 5.0, "jobs");
 
         return result;
     }
 
-    void addConfiguration(
-            int index, double lambda, double z, double cv, int maxWsNumber,
-            Double siMax, Double scalingThreshold,
-            Double fastLambda
-    ) {
-        RunConfiguration c = new RunConfiguration("trans_base_" + String.format("%02d", index));
+    void setConfiguration(int index, int maxWsNum, double siMax, Double scalingThr, String scalingType) {
+        //        RunConfiguration c = new RunConfiguration("base_1_" + String.format("%02d", index));
+        RunConfiguration c = new RunConfiguration(String.valueOf(index));
 
-        /* Common */
-        c.put("random.repeat_config", "1");
+        /* For transient */
+        c.put("log.fine", "false");
         c.put("log.intra_run", "true");
+        c.put("random.repeat_config", "64");
         c.put("stats.batch.size", "INFINITY");
-        c.put("system.stop", "10000");
+        c.put("system.stop", "15000");
         c.put("system.empty_jobs", "false");
 
-        /* Specific */
-        c.put("distribution.arrivals.mu", String.valueOf(1.0 / lambda));
-        c.put("distribution.arrivals.cv", String.valueOf(cv));
-        c.put("distribution.services.z", String.valueOf(z));
-        c.put("distribution.services.cv", String.valueOf(cv));
-        c.put("infrastructure.max_num_server", String.valueOf(maxWsNumber));
-        c.put("infrastructure.spikeserver.active", String.valueOf(siMax != null));
-        c.put(
-                "infrastructure.si_max",
-                (siMax == null) ? "-1.0" : String.valueOf(siMax)
-        );
-        c.put(
-                "webserver.scaling.out_thr",
-                (scalingThreshold == null) ? "INFINITY" : String.valueOf(scalingThreshold)
-        );
+        /* Common */
+        c.put("distribution.arrivals.type", "h2");
+        c.put("distribution.services.type", "h2");
+        c.put("distribution.arrivals.mu", String.valueOf(1.0 / 4.0));
+        c.put("distribution.services.z", "1");
+        c.put("infrastructure.spikeserver.active", "true");
+        c.put("distribution.arrivals.fast_mu", String.valueOf(1.0 / 8.0));
+        c.put("distribution.arrivals.fast_interval", String.valueOf(100.0));
+        c.put("spikeserver.version", "2");
 
-        /* Add configuration with long-term fluctuations */
-        if (fastLambda != null) {
-            c.put("distribution.arrivals.fast_interval", "100");
-            c.put("distribution.arrivals.fast_mu", String.valueOf(1.0 / fastLambda));
-        }
+        /* Specific */
+        c.put("infrastructure.max_num_server", String.valueOf(maxWsNum));
+        c.put("infrastructure.si_max", String.valueOf(siMax));
+        c.put("webserver.scaling.out_thr", String.valueOf(scalingThr));
+        c.put("webserver.scaling.type", scalingType);
 
         // extra
-        c.put("infrastructure.start_num_server", String.valueOf(maxWsNumber / 2));
+        c.put("infrastructure.start_num_server", String.valueOf(1));
 
         result.add(c);
     }
